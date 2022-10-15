93443dac8535d39193359a520c26d8b36ea27f9e
kadirc <kadir@protonmail.com>
2022-10-15 23:58:23 +0200

    (fix) Wait until conversion rates are available

--- hummingbot/strategy/cross_exchange_market_making/cross_exchange_market_making.py
+++ hummingbot/strategy/cross_exchange_market_making/cross_exchange_market_making.py
@@ -100,6 +100,7 @@ class CrossExchangeMarketMakingStrategy(StrategyPyBase):
         self._maker_markets = set([market_pair.maker.market for market_pair in market_pairs])
         self._taker_markets = set([market_pair.taker.market for market_pair in market_pairs])
         self._all_markets_ready = False
+        self._conversions_ready = False
 
         self._anti_hysteresis_timers = {}
         self._order_fill_buy_events = {}
@@ -249,14 +250,11 @@ class CrossExchangeMarketMakingStrategy(StrategyPyBase):
         quote_pair, quote_rate_source, quote_rate, base_pair, base_rate_source, base_rate, gas_pair, gas_rate_source,\
             gas_rate = self._config_map.conversion_rate_mode.get_conversion_rates(market_pair)
         if quote_rate is None:
-            self.logger().warning(f"Can't find a conversion rate for {quote_pair}, using 1.0 instead.")
-            quote_rate = Decimal("1")
+            self.logger().warning(f"Can't find a conversion rate for {quote_pair}")
         if base_rate is None:
-            self.logger().warning(f"Can't find a conversion rate for {base_pair}, using 1.0 instead.")
-            base_rate = Decimal("1")
+            self.logger().warning(f"Can't find a conversion rate for {base_pair}")
         if gas_rate is None:
-            self.logger().warning(f"Can't find a conversion rate for {gas_pair}, using 1.0 instead.")
-            gas_rate = Decimal("1")
+            self.logger().warning(f"Can't find a conversion rate for {gas_pair}")
         return quote_pair, quote_rate_source, quote_rate, base_pair, base_rate_source, base_rate, gas_pair,\
             gas_rate_source, gas_rate
 
@@ -1531,7 +1529,10 @@ class CrossExchangeMarketMakingStrategy(StrategyPyBase):
         Return price conversion rate for from taker quote asset to maker quote asset
         """
         _, _, quote_rate, _, _, base_rate, _, _, _ = self.get_conversion_rates(market_pair)
-        return quote_rate / base_rate
+        if quote_rate and base_rate:
+            return quote_rate / base_rate
+        else:
+            return None
         # else:
         #     market_pairs = list(self._market_pairs.values())[0]
         #     quote_pair = f"{market_pairs.taker.quote_asset}-{market_pairs.maker.quote_asset}"
